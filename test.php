4d21f6806aff316db2d15cd7699a8c57d1f2b644
Xetolone <thomas.beline@gmail.com>
2015-03-02 00:50:42 +0100

    Add english test, minor mofifications

--- test.php
+++ test.php
@@ -1,13 +1,28 @@
 <?php
 require 'typography.php';
 
-$text = <<<EOT
-voici un texte , avec.. un nombre impressionnant
+error_reporting(E_ALL | E_STRICT);
+ini_set("display_errors", 1);
+
+$text_fr = <<<EOT
+voici un texte , avec.. un nombre impressionnnnnnant
 de fautes!?!! c'est pas ma soeur qui dira le contraire:
-ca marche "correctement"
+ca marche "correctement".
+huhu [test] 4 36
 EOT;
 
-echo $text;
-echo "\n***\n";
-echo typography($text, "fr");
+$text_en = <<<EOT
+this is , an example in   : english....
+but i'm not a good "writer" !
+huhu [test] 4 36
+EOT;
+
+echo " == Test en français :\n";
+echo $text_fr;
+echo "\n == Correction :\n";
+echo typography($text_fr, "fr");
+echo "\n == Test in english:\n";
+echo $text_en;
+echo "\n == Correction:\n";
+echo typography($text_en, "en");
 ?>
